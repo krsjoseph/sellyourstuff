835d64e61f142e2373b2db4fddcc2fa12c3a2732
Krs Joseph <krspjoseph@gmail.com>
2015-04-21 13:46:35 -0400

    Add posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421164412) do
+ActiveRecord::Schema.define(version: 20150421173506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20150421164412) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "location"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/factories/listings.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :listing do
+    name "MyString"
+description "MyText"
+location "MyString"
+  end
+
+end
