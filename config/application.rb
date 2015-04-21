2287186df4b0d58bfdcf3097e2f43f5c26e595b5
Krs Joseph <krspjoseph@gmail.com>
2015-04-21 16:15:59 -0400

    Add pages theme

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module Sellyourstuff
     end
 
     config.action_controller.action_on_unpermitted_parameters = :raise
+    config.assets.paths << File.join(Rails.root, "/vendor/pages")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -37,6 +38,7 @@ module Sellyourstuff
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'La Paz'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
