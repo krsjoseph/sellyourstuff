5bd683eb2db66f41f4991c7926f638a8e5e50727
Krs Joseph <krspjoseph@gmail.com>
2015-04-21 16:40:07 -0400

    Update root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :listings
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root to: "pages#index"
+  root to: "listings#index"
+
 end
