3cccc1a53b3e25f6a6901ce7f5c2f018854d7e3d
Krs Joseph <krspjoseph@gmail.com>
2015-04-22 15:46:41 -0400

    Update listing index and geallery

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,10 @@ class Listing < ActiveRecord::Base
   belongs_to :user
   has_many :pictures, :dependent => :destroy
 
-  has_attached_file :image, :styles => { :featured => "350x350#", :thumb => "100x100>" }, :default_url => "http://longwhiteclouds.com/wp-content/themes/longwhiteclouds/images/missing-image-640x360.png"
+  has_attached_file :image, :styles => { :wide => "463x387>",
+                                         :medium => "225x188>",
+                                         :thumb => "240×240#",
+                                         :big => "575x575#",},
+                    :default_url => "http://longwhiteclouds.com/wp-content/themes/longwhiteclouds/images/missing-image-640x360.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- vendor/pages/pages-core/js/gallery.js
+++ vendor/pages/pages-core/js/gallery.js
@@ -7,54 +7,68 @@
 $(function() {
 
     /* GRID
-    -------------------------------------------------------------*/
+     -------------------------------------------------------------*/
 
     /* 
-        Wait for the images to be loaded before applying
-        Isotope plugin. 
-    */
+     Wait for the images to be loaded before applying
+     Isotope plugin.
+     */
     var $gallery = $('.gallery');
     $gallery.imagesLoaded(function() {
         applyIsotope();
     });
 
     /*  Apply Isotope plugin 
-        isotope.metafizzy.co
-    */
+     isotope.metafizzy.co
+     */
+
+    // 1170/3 = 390px (per column)
+// columnWidth = 390 - gutter
     var applyIsotope = function() {
         $gallery.isotope({
             itemSelector: '.gallery-item',
             masonry: {
-                columnWidth: 280,
+                columnWidth: 282.5,
                 gutter: 10,
                 isFitWidth: true
             }
         });
     }
-    
+
+    //var applyIsotope = function() {
+    //    $gallery.isotope({
+    //        itemSelector: '.gallery-item',
+    //        masonry: {
+    //            columnWidth: 280,
+    //            gutter: 10,
+    //            isFitWidth: true
+    //        }
+    //    });
+    //}
+
     /*
-        Show a sliding item using MetroJS
-        http://www.drewgreenwell.com/projects/metrojs
-    */
+     Show a sliding item using MetroJS
+     http://www.drewgreenwell.com/projects/metrojs
+     */
     $(".live-tile,.flip-list").liveTile();
 
 
-     /* DETAIL VIEW
-    -------------------------------------------------------------*/
+    /* DETAIL VIEW
+     -------------------------------------------------------------*/
 
     /*
-        Toggle detail view using DialogFx
-        http://tympanus.net/Development/DialogEffects/
-    */
-    $('body').on('click', '.gallery-item', function() {
-        var dlg = new DialogFx($('#itemDetails').get(0));
-        dlg.toggle();
-    });
+     Toggle detail view using DialogFx
+     http://tympanus.net/Development/DialogEffects/
+     */
+    //$('body').on('click', '.gallery-item', function() {
+    //    var dlg = new DialogFx($('#itemDetails').get(0));
+    //    dlg.toggle();
+    //});
 
     /*
-        Look for data-image attribute and apply those
-        images as CSS background-image 
-    */
+     Look for data-image attribute and apply those
+     images as CSS background-image
+     */
     $('.item-slideshow > div').each(function() {
         var img = $(this).data('image');
         $(this).css({
@@ -64,9 +78,9 @@ $(function() {
     });
 
     /* 
-        Touch enabled slideshow for gallery item images using owlCarousel
-        www.owlcarousel.owlgraphic.com
-    */
+     Touch enabled slideshow for gallery item images using owlCarousel
+     www.owlcarousel.owlgraphic.com
+     */
     $(".item-slideshow").owlCarousel({
         items: 1,
         nav: true,
@@ -75,8 +89,8 @@ $(function() {
     });
 
 
-     /* FILTERS OVERLAY
-    -------------------------------------------------------------*/
+    /* FILTERS OVERLAY
+     -------------------------------------------------------------*/
 
     $('[data-toggle="filters"]').click(function() {
         $('#filters').toggleClass('open');
