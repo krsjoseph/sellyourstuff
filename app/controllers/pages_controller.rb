e427c10e30af5b981f0fcebce1780675dd8d13d6
Krs Joseph <krspjoseph@gmail.com>
2015-04-22 10:56:04 -0400

    Update style of homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @listings = Listing.all.order("created_at DESC")
+    @listings = Listing.last(3)
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,6 @@ class Listing < ActiveRecord::Base
   belongs_to :user
   has_many :pictures, :dependent => :destroy
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://longwhiteclouds.com/wp-content/themes/longwhiteclouds/images/missing-image-640x360.png"
+  has_attached_file :image, :styles => { :featured => "350x350#", :thumb => "100x100>" }, :default_url => "http://longwhiteclouds.com/wp-content/themes/longwhiteclouds/images/missing-image-640x360.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
