876346aa84cdb7392e441cec321d032e8a130e33
Krs Joseph <krspjoseph@gmail.com>
2015-04-22 02:25:38 -0400

    Add hero image

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :listings
   resources :pictures
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root to: "listings#index"
-
+  root to: "pages#index"
 end
